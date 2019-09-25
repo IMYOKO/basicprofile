2919fd68849591df77c655896abc6d3e222214a9
YOKO <yoko.wang@makeblock.com>
2019-09-26 00:03:32 +0800

    新增一些配置

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
-import styles from './index.less';
+import React, { Component } from 'react';
+import { Badge, Card, Descriptions, Divider, Table } from 'antd';
+import { PageHeaderWrapper } from '@ant-design/pro-layout';
+import styles from './style.less';
 
-const Block: React.FC = () => {
-  return (
-    <div className={styles.normal}>
-      <h1>I am a umi block!sdsd</h1>
-    </div>
-  );
+class Block extends Component {
+  render() {
+    return(
+      <PageHeaderWrapper>
+        <Card bordered={false}>
+          <Descriptions title="退款申请" style={{ marginBottom: 32 }}>
+            <Descriptions.Item label="取货单号">1000000000</Descriptions.Item>
+            <Descriptions.Item label="状态">已取货</Descriptions.Item>
+            <Descriptions.Item label="销售单号">1234123421</Descriptions.Item>
+            <Descriptions.Item label="子订单">3214321432</Descriptions.Item>
+          </Descriptions>
+        </Card>
+      </PageHeaderWrapper>
+    )
+  }
 }
 
 export default Block;
